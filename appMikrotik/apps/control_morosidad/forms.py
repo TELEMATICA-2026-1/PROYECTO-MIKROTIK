a00aa912af529a2c13c8c7d05bf1767daa40f163
TeleAlfred <alfredojoseffx@gmail.com>
2026-06-15 19:03:23 -0400

    Se ajusta el forms.py

--- appMikrotik/apps/control_morosidad/forms.py
+++ appMikrotik/apps/control_morosidad/forms.py
@@ -7,6 +7,21 @@ class ConfiguracionMorosidadForm(forms.ModelForm):
         model = ConfiguracionMorosidad
         fields = ['diasGracia','diaCobroMensual']
         widgets={
-            'diasGracia': forms.NumberInput(attrs={'class': 'form-control'}),
+            'diasGracia': forms.NumberInput(attrs={'class': 'form-control', 'min':1, 'max':30}),
             'diaCobroMensual': forms.NumberInput(attrs={'class': 'form-control', 'min':1, 'max':28}),
         }
+    
+    #validaciones para asegurar que los valores ingresados sean validos, aunque el widget ya limita el rango, esto es una capa adicional de seguridad
+    def clean_diaCobroMensual(self):
+        diaCobro = self.cleaned_data['diaCobroMensual']
+        if diaCobro < 1 or diaCobro > 28:
+            raise forms.ValidationError("El día de cobro mensual debe estar entre 1 y 28.")
+        return diaCobro
+    
+    def clean_diasGracia(self):
+        diasGracia = self.cleaned_data['diasGracia']
+        if diasGracia < 1 or diasGracia > 30:
+            raise forms.ValidationError("Los días de gracia deben estar entre 1 y 30.")
+        return diasGracia
+    
+    
